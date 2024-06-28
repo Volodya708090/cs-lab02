a53d65311ee7e60bc3201a11d47ad804513ec84e
Bob (OvsyannikovVV) <bob@example.com>
2024-06-28 22:52:28 +0300

    добавлен минимум чисел

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
+int main() {
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B =" << a + b << '\n'
-         << "A - B =" << a - b << '\n'
-         << "A * B =" << a * b << '\n';
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n'
+         << "min(A, B) = " << min(a, b) << '\n';
     return 0;
 }
